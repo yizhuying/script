57227c98fe974a848ac5ed399ed4e593de42ecc2
Naeshaun Medal <122763218+yizhuying@users.noreply.github.com>
2025-10-09 12:41:28 +0800

    Update ecovacs_menu.js

--- script/ecovacs_menu.js
+++ script/ecovacs_menu.js
@@ -5,7 +5,34 @@
  * 时间：12:37
  */
 
+const isNetEase= true;
 
-const cookie = $argument?.Cookie;
-console.log("✅ 网易云音乐会员已解锁 🎉","cookie");
-$done({});
+if (isNetEase) {
+  // 从 Loon 脚本参数中读取配置
+  const cookie = $argument?.Cookie;
+  const mconfig = $argument?.MConfigInfo;
+  const userAgent = $argument?.UserAgent;
+
+  // 检查参数是否缺失
+  if (!cookie || !mconfig || !userAgent) {
+        console.log("参数缺失信息：");
+    if (!cookie) console.log("❌ Cookie 参数缺失");
+    if (!mconfig) console.log("❌ MConfigInfo 参数缺失");
+    if (!userAgent) console.log("❌ UserAgent 参数缺失");
+    
+    $notification.post("网易云音乐遇到问题", "参数缺失", "请在插件内填入会员数据");
+    $done({});
+  } else {
+    header["cookie"] = cookie;
+    header["mconfig-info"] = mconfig;
+    header["user-agent"] = userAgent;
+
+
+    console.log("cookie:",cookie);
+    
+    console.log("✅ 网易云音乐会员已解锁 🎉");
+    $done({ headers: header });
+  }
+} else {
+  $done({});
+}
